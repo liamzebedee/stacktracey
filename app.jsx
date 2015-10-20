9c1c572d924b524109ff91495ac310c9e1b7e71b
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2015-10-20 13:56:57 +1100

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -57,8 +57,7 @@ var StackTrace = React.createClass({
       var regexStack = "(!at )?(?<stack>"+escapeRegExp(this.state.currentHighlight)+")";
       var regexAhead = "(?<ahead>[\\w\\d\\<\\>\\_\\.\\[\\]\\`\\<\\>\\_]*(?![0-9\\w\\,\\`\\s\\&\\[\\]]*[\\(]))";
       var regexMethodAndParams = "(?<methodAndParams>[\\w\\.]*\\([0-9\\w\\,\\`\\s\\&\\[\\]]*\\))";
-      var regexOrMatchTheEntireLine = "|^" + escaoeRegExp(this.state.currentHighlight);
-      regex = regexStack + regexAhead + regexMethodAndParams + regexOrMatchTheEntireLine;
+      regex = regexStack + regexAhead + regexMethodAndParams;
     }
     var magicRegex = XRegExp(regex);
     
